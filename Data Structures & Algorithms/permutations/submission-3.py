4ee3443f4ce37feeac6770a7549cd9fc93351b0c
Yassine Kraiem <148379527+Yassinekraiem08@users.noreply.github.com>
2026-09-13 18:52:05 -0400

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        output = []
+        permutation = []
+
+        def backtrack(i):
+            if len(permutation) == len(nums):
+                output.append(permutation.copy())
+                return
+            
+            for i in range(len(nums)):
+                if nums[i] in permutation:
+                    continue
+                permutation.append(nums[i])
+                backtrack(i + 1)
+                permutation.pop()
+
+            
+        backtrack(0)
+        return output
+            
